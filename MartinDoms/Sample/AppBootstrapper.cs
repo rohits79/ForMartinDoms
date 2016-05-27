3b993318576707a200f42949e33de033aef6a7fa
rohits79 <rohits79@gmail.com>
2016-05-27 09:37:02 +0800

    First commit showing data template with virtualized listbox works just fine with CM

---
+++ MartinDoms/Sample/AppBootstrapper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using AutoDumper.ViewModels;
+using Autofac;
+using Autofac.Features.ResolveAnything;
+using Caliburn.Micro;
+
+namespace AutoDumper
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+        
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override void Configure()
+        { 
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray()).
+                Where(x => x.Name.EndsWith("ViewModel")).
+                Where(x => !(string.IsNullOrWhiteSpace(x.Namespace)) && x.Namespace.EndsWith("ViewModels")).
+                Where(x => x.GetInterface(typeof (INotifyPropertyChanged).Name) != null).
+                AsSelf().
+                InstancePerDependency();
+
+            builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray()).
+                Where(x => x.Name.EndsWith("View")).
+                Where(x => !(string.IsNullOrWhiteSpace(x.Namespace)) && x.Namespace.EndsWith("Views")).
+                AsSelf().
+                InstancePerDependency();
+
+            builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+            builder.Register<IWindowManager>(c => new WindowManager()).InstancePerLifetimeScope();
+         
+            builder.Register<IEventAggregator>(c => new EventAggregator()).InstancePerLifetimeScope();
+
+            _container = builder.Build();
+        }
+
+        
+        
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            if (!String.IsNullOrWhiteSpace(key))
+            {
+                object instance = null;
+                if (Container.TryResolveKeyed(key, serviceType, out instance))
+                {
+                    return instance;
+                }
+            }
+
+            if (Container.IsRegistered(serviceType))
+            {
+                return Container.Resolve(serviceType);
+            }
+
+            throw new Exception($"Could not locate any instances of contract {key ?? serviceType.Name}.");
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            
+            return Container.Resolve(typeof(IEnumerable<>).MakeGenericType(serviceType)) as IEnumerable<object>;
+        }
+
+        private Autofac.IContainer _container;
+        protected Autofac.IContainer Container => _container;
+    }
+}
---
+++ MartinDoms/Sample/ViewModels/ShellViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Caliburn.Micro;
+using Action = System.Action;
+
+namespace AutoDumper.ViewModels
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+        public IEnumerable<ItemViewModel> Items { get; set; }
+
+        public ShellViewModel()
+        {
+         
+            Items = new List<ItemViewModel>
+                        {
+                            new ItemViewModel("1"),
+                            new ItemViewModel("2"),
+                            new ItemViewModel("3"),
+                            new ItemViewModel("4"),
+                            new ItemViewModel("5"),
+                            new ItemViewModel("6"),
+                            new ItemViewModel("7"),
+                            new ItemViewModel("8"),
+                            new ItemViewModel("9"),
+                            new ItemViewModel("10"),
+                        };
+        }
+    }
+
+    public class ItemViewModel : Screen
+    {
+        public string S { get; private set; }
+        public ItemViewModel(string s)
+        {
+            S = s;
+        }
+        public void DoThings()
+        {
+            Debug.WriteLine(S);
+        }
+    }
+}
